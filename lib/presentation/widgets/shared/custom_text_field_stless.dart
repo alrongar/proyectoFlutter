eec19b668ed2054a13dc331842df0f165cbe19a7
Fernando <bares.gafer23@cadiz.salesianos.edu>
2024-10-23 21:46:54 +0200

    correcion customTextFieldStateless

--- lib/presentation/widgets/shared/custom_text_field_stless.dart
+++ lib/presentation/widgets/shared/custom_text_field_stless.dart
@@ -1,39 +1,23 @@
 import 'package:flutter/material.dart';
-
-class CustomTextFieldStful extends StatelessWidget {
+class CustomTextField extends StatelessWidget {
   final TextEditingController textController = TextEditingController();
   final String hintTextContent;
   final bool isPassword;
-  final bool isRequired;
-
-  CustomTextFieldStful({
-    super.key,
-    this.hintTextContent = '',
-    this.isPassword = false,
-    this.isRequired = false,
-  });
-
+  CustomTextField(
+      {super.key, this.hintTextContent = '', this.isPassword = false});
   @override
   Widget build(BuildContext context) {
     final FocusNode focusNode = FocusNode();
     final UnderlineInputBorder outlineInputBorder = UnderlineInputBorder(
-      borderSide: const BorderSide(color: Colors.transparent),
-      borderRadius: BorderRadius.circular(40),
-    );
-
+        borderSide: const BorderSide(color: Colors.transparent),
+        borderRadius: BorderRadius.circular(40));
     final inputDecoration = InputDecoration(
       hintText: hintTextContent,
       enabledBorder: outlineInputBorder,
       focusedBorder: outlineInputBorder,
-      errorBorder: outlineInputBorder.copyWith(
-        borderSide: const BorderSide(color: Colors.red),
-      ),
-      focusedErrorBorder: outlineInputBorder.copyWith(
-        borderSide: const BorderSide(color: Colors.red),
-      ),
       filled: true,
     );
-
+    
     return TextFormField(
       onTapOutside: (event) {
         focusNode.unfocus();
@@ -42,14 +26,7 @@ class CustomTextFieldStful extends StatelessWidget {
       controller: textController,
       decoration: inputDecoration,
       obscureText: isPassword,
-      validator: (value) {
-        if (isRequired && (value == null || value.isEmpty)) {
-          return 'Este campo es obligatorio';
-        }
-        return null;
-      },
     );
   }
-
   String get textValue => textController.value.text;
 }
