2ce8066731379fae41810fc18881cb03ec8f9231
alrongar <alejandroroncerogarrido@gmail.com>
2024-10-31 19:33:52 +0100

    estilo provisional pantalla administrador

--- lib/presentation/screens/users/admin_user_screen.dart
+++ lib/presentation/screens/users/admin_user_screen.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
+import 'package:eventify_flutter/presentation/widgets/shared/gradient_background.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
 import '../../../providers/UserService.dart';
 
 class UserListScreen extends StatefulWidget {
@@ -37,26 +37,45 @@ class _UserListScreenState extends State<UserListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Administración de Usuarios'),
-        iconTheme: const IconThemeData(color: Colors.white),
-        backgroundColor: const Color(0xff620091),
-      ),
-      body: users.isEmpty
-          ? const Center(child: CircularProgressIndicator())
-          : ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemCount: users.length,
-        itemBuilder: (context, index) {
-          var user = users[index];
-          return Card(
-            margin: const EdgeInsets.symmetric(vertical: 8.0),
-            child: ListTile(
-              title: Text(user['name'] ?? 'Nombre no disponible'), // Solo muestra el nombre
-            ),
-          );
-        },
-      ),
-    );
+        appBar: AppBar(
+            title: const Text('Administración de Usuarios'),
+            iconTheme: const IconThemeData(color: Colors.white),
+            backgroundColor: const Color.fromARGB(255, 44, 60, 75),
+            centerTitle: true,
+            titleTextStyle: const TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            )),
+        body: users.isEmpty
+            ? const Center(child: CircularProgressIndicator())
+            : GradientBackground(
+                title: '',
+                child: ListView.builder(
+                  padding: const EdgeInsets.all(16.0),
+                  itemCount: users.length,
+                  itemBuilder: (context, index) {
+                    var user = users[index];
+                    var name = user['name'];
+                    var email = user['email'];
+                    var role = user['role'];
+                    if (role == 'u') {
+                      role = 'Usuario';
+                    } else if (role == 'o') {
+                      role = 'Organizador';
+                    }
+                    return Card(
+                      margin: const EdgeInsets.symmetric(vertical: 8.0),
+                      color: const Color.fromARGB(255, 82, 112, 138),
+                      
+                      child: ListTile(
+                        title: Text('$name || Email: $email || Rol: $role' ??
+                            'Nombre no disponible'),
+                        textColor: const Color.fromARGB(255, 255, 255, 255),
+                      ),
+                    );
+                  },
+                ),
+              ));
   }
 }
