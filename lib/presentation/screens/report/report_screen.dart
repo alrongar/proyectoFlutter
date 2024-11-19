724114430538e1762a49593debb42e9ff9a994d0
Fernando <bares.gafer23@cadiz.salesianos.edu>
2024-11-19 10:15:56 +0100

    Correciones generar pdf

--- lib/presentation/screens/report/report_screen.dart
+++ lib/presentation/screens/report/report_screen.dart
@@ -1,12 +1,14 @@
-// ignore_for_file: library_private_types_in_public_api
+// ignore_for_file: library_private_types_in_public_api, use_build_context_synchronously
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:path_provider/path_provider.dart' as path_provider;
 import 'dart:io';
 import '../../../providers/event_service.dart';
 import '../../../models/event.dart';
+import '../../../models/category.dart';
 
 class ReportScreen extends StatefulWidget {
   const ReportScreen({super.key});
@@ -20,13 +22,32 @@ class _ReportScreenState extends State<ReportScreen> {
   DateTime? _startDate;
   DateTime? _endDate;
   final List<String> _selectedCategories = [];
-  final List<String> _categories = ['Music', 'Sport', 'Technology'];
+  List<Category> _categories = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchCategories();
+  }
+
+  Future<void> _fetchCategories() async {
+    try {
+      List<Category> categories = await EventServices().fetchCategories();
+      setState(() {
+        _categories = categories;
+      });
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Error al cargar las categorías')),
+      );
+    }
+  }
 
   Future<void> _selectDate(BuildContext context, bool isStart) async {
     final DateTime? picked = await showDatePicker(
       context: context,
       initialDate: DateTime.now(),
-      firstDate: DateTime(2000),
+      firstDate: DateTime(DateTime.now().year), // Usar el año actual
       lastDate: DateTime(2101),
     );
     if (picked != null && picked != (isStart ? _startDate : _endDate)) {
@@ -41,6 +62,13 @@ class _ReportScreenState extends State<ReportScreen> {
   }
 
   Future<void> _generatePDF() async {
+    if (_startDate == null || _endDate == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Por favor, seleccione las fechas de inicio y fin')),
+      );
+      return;
+    }
+
     if (_formKey.currentState?.validate() ?? false) {
       _formKey.currentState?.save();
 
@@ -58,26 +86,44 @@ class _ReportScreenState extends State<ReportScreen> {
         return matchesDate && matchesCategory;
       }).toList();
 
+      // Verificar si hay eventos
+      if (events.isEmpty) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('No hay eventos que cumplan con los criterios seleccionados')),
+        );
+        return;
+      }
+
       // Generate PDF
       final pdf = pw.Document();
-      pdf.addPage(
-        pw.Page(
-          build: (pw.Context context) {
-            return pw.Column(
-              children: events.map((event) {
-                return pw.Text('${event.title} - ${DateFormat('dd/MM/yyyy').format(event.startTime)}');
-              }).toList(),
-            );
-          },
-        ),
-      );
+      final List<pw.Widget> eventWidgets = [];
+      for (var event in events) {
+        final image = pw.MemoryImage(
+          (await NetworkAssetBundle(Uri.parse(event.imageUrl)).load(event.imageUrl)).buffer.asUint8List(),
+        );
+        eventWidgets.add(
+          pw.Column(
+            crossAxisAlignment: pw.CrossAxisAlignment.start,
+            children: [
+              pw.Image(image, width: 100, height: 100),
+              pw.Text(event.title, style: pw.TextStyle(fontSize: 18, fontWeight: pw.FontWeight.bold)),
+              pw.Text('${DateFormat('dd/MM/yyyy').format(event.startTime)} - ${DateFormat('dd/MM/yyyy').format(event.startTime)}'),
+              pw.SizedBox(height: 10),
+            ],
+          ),
+        );
+      }
+      pdf.addPage(pw.Page(build: (pw.Context context) => pw.Column(children: eventWidgets)));
 
-      // Save PDF to internal storage
-      final directory = await path_provider.getApplicationDocumentsDirectory();
-      final file = File('${directory.path}/report.pdf');
+      // Save PDF to Download directory
+      final directory = await path_provider.getExternalStorageDirectory();
+      final downloadDir = Directory('${directory!.path}/Download');
+      if (!await downloadDir.exists()) {
+        await downloadDir.create(recursive: true);
+      }
+      final file = File('${downloadDir.path}/report.pdf');
       await file.writeAsBytes(await pdf.save());
 
-      // ignore: use_build_context_synchronously
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('PDF generado y guardado en ${file.path}')),
       );
@@ -108,19 +154,19 @@ class _ReportScreenState extends State<ReportScreen> {
               ),
               ..._categories.map((category) {
                 return CheckboxListTile(
-                  title: Text(category),
-                  value: _selectedCategories.contains(category),
+                  title: Text(category.name),
+                  value: _selectedCategories.contains(category.name),
                   onChanged: (bool? value) {
                     setState(() {
                       if (value == true) {
-                        _selectedCategories.add(category);
+                        _selectedCategories.add(category.name);
                       } else {
-                        _selectedCategories.remove(category);
+                        _selectedCategories.remove(category.name);
                       }
                     });
                   },
                 );
-              }),
+              }).toList(),
               const SizedBox(height: 20),
               ElevatedButton(
                 onPressed: _generatePDF,
